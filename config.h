503367b00e92102304d138b1e40aabca4f136d07
Viraj Desai <virajpd19@gmail.com>
2020-05-11 17:48:18 -0400

    updated fonts

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 15;       /* gap pixel between windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Noto Sans:size=10" };
-static const char dmenufont[]       = "Noto Sans:size=10";
+static const char *fonts[]          = { "DejaVuSansCondensed:size=10" };
+static const char dmenufont[]       = "DejaVuSansCondensed:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
