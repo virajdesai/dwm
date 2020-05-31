9f2039f936964d3a27cf5e9dd14bd39454f0aac0
Viraj Desai <virajpd19@gmail.com>
2020-05-31 10:38:37 -0400

    dwmblocks

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,9 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "~/.config/dwmblocks/blocks/audio.sh", 60,	9},
+	{"", "~/.config/dwmblocks/blocks/time.sh", 60, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = ' ';
---
+++ dwmblocks/blocks/time.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+printf '%s%s   %s%s  \n' "" "$(date "+%a %d %b %Y")" "" "$(date "+%I:%M %p")"
